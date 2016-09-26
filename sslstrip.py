23ae1caca184a3ff0378c2eacca74e6d792ab006
kcrong <kcrong@kim82536.pe.kr>
2016-09-26 15:46:06 +0900

    Add setting code

--- sslstrip.py
+++ sslstrip.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 from twisted.internet import reactor
@@ -6,7 +7,28 @@ from twisted.web import http
 from StrippingProxy import StrippingProxy
 
 
+class Setting:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def __exec_shell(command):
+        return os.popen(command).read()
+
+    def enable_ip_forwarding(self):
+        self.__exec_shell('echo "1" > /proc/sys/net/ipv4/ip_forward')
+
+    def disable_ip_forwarding(self):
+        self.__exec_shell('echo "0" > /proc/sys/net/ipv4/ip_forward')
+
+
 def main():
+    if 0 == os.getuid():
+        pass
+    else:
+        print("We need root permission")
+        exit()
+
     # Using Twisted
     logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(message)s',
                         filename='sslstrip.log', filemode='w')
